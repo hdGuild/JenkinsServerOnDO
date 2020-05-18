4a182a205695664c3ca29fe15c6ab1215d932900
hdGuild <contact.laguilde@helldorado.fr>
2020-05-18 15:14:59 +0200

    ending the jenkins_init merge

--- build/golden/terraform/variables.tf
+++ build/golden/terraform/variables.tf
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 # fullpath for pat and ssh security files that stay on workstation 
 variable "fullpath_do_token_file" {}
 variable "fullpath_pub_key_file" {}
@@ -26,32 +25,3 @@ variable "server_root_name" {}
 variable "jenkinsDomainName" {}
 variable "jenkinsServerName" {}
 variable "jenkinsFirewallName" {}
-=======
-# fullpath for pat and ssh security files that stay on workstation 
-variable "fullpath_do_token_file" {}
-variable "fullpath_pub_key_file" {}
-variable "fullpath_priv_key_file" {}
-variable "fullpath_ssh_fingerprint_file" {}
-
-# generic variables
-variable "region" {}
-variable "environment" {}
-variable "connectionType" {}
-variable "ssh_key_id" {}
-
-# variable for jenkins server install
-variable "server_image" {}
-variable "server_name" {}
-variable "server_size" {}
-
-# installation scripts
-variable "initial_server_setup_script" {}
-variable "jenkins_install_script" {}
-variable "disallow_remote_root_login_script" {}
-#variable "server_host_url" {}
-## connection to use for provisioning
-variable "server_root_name" {}
-variable "jenkinsDomainName" {}
-variable "jenkinsServerName" {}
-variable "jenkinsFirewallName" {}
->>>>>>> Stashed changes
