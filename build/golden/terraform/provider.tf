0697e44731731283e501d247f540a9ed6b85d7c7
hdGuild <contact.laguilde@helldorado.fr>
2024-01-22 17:01:36 +0100

    comment backend

--- build/golden/terraform/provider.tf
+++ build/golden/terraform/provider.tf
@@ -5,20 +5,18 @@ provider "digitalocean" {
   version = "~> 1.0"
 }
 
-# Waiting for terrafomr backend to store tfstate file
-terraform {
-  backend "s3" {
-    endpoint = "ams3.digitaloceanspaces.com"
-    region = "eu-central-1"
-    bucket = "hdguildspace"
-    key = "tfstate/jenkins-srv.tfstate"
-    access_key = "LXCT7DI4XEB6FKV5FMFG"
-    #secret_key = file("C:\\Users\\Philippe\\.sak\\HDGuild_tfstate.secret") 
-    # call the terraform init with the following parameter to set the secret :
-    ## terraform init -backend-config="secret_key=<HDGuild_tfstate.secret content>"   
-    skip_requesting_account_id = true
-    skip_credentials_validation = true
-    skip_get_ec2_platforms = true
-    skip_metadata_api_check = true
-  }
-}
+# This terraform backup works well
+# to be used in production
+# terraform {
+#   backend "s3" {
+#     endpoint = "ams3.digitaloceanspaces.com"
+#     region = "eu-central-1"
+#     bucket = "hdguildspace"
+#     key = "tfstate/jenkins-srv.tfstate"
+#     access_key = "LXCT7DI4XEB6FKV5FMFG"
+#     skip_requesting_account_id = true
+#     skip_credentials_validation = true
+#     skip_get_ec2_platforms = true
+#     skip_metadata_api_check = true
+#   }
+# }
